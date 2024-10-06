5ea05b665218c58e1711e174b55f05e07bc9b216
Yousef Jarbou <166923297+yousefjarbou@users.noreply.github.com>
2024-10-06 15:00:47 +0300

    Create vacancy.dart

---
+++ FlutterApp/vacancy.dart
@@ -0,0 +1,19 @@
+//{'squat': 0, 'biceps_right': 3, 'biceps_left': 3, 'triceps_right': 1, 'triceps_left': 1, 'back': 2}
+class Exercise{
+  final String squat;
+  final String biceps_right;
+  final String biceps_left;
+  final String triceps_right;
+  final String triceps_left;
+  final String back;
+  Exercise(
+  {
+    required this.squat,
+    required this.biceps_right,
+    required this.biceps_left,
+    required this.triceps_right ,
+    required this.triceps_left ,
+    required this.back ,
+  }
+      );
+}
